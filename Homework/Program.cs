d56eef4a5ba98adeed84834faed94ee4713a2102
Abbas <Abbas@DESKTOP-C9MPOKJ>
2025-01-22 17:32:50 +0400

    Add project files.

---
+++ Homework/Program.cs
@@ -0,0 +1,175 @@
+﻿//Console.Write("Enter the number:");
+//double a = Convert.ToDouble(Console.ReadLine());
+//Console.Write("Enter the second number:");
+//double b = Convert.ToDouble(Console.ReadLine());
+//double sum = a + b;
+//Console.Write("Answer:" + sum);
+
+
+
+//Console.Write("Enter the number: ");
+//int a = int.Parse(Console.ReadLine());
+//if (a % 2 == 0)
+//{
+//    Console.Write("Reqem cutdur.");
+//}
+//else
+//{
+//    Console.Write("Reqem tekdir.");
+//}
+
+
+
+//Console.Write("Enter the number:");
+//int a=int.Parse(Console.ReadLine());
+//if (a > 0)
+//{
+//    Console.Write("Number is positive");
+//}
+//else if (a < 0)
+//{
+//    Console.Write("Number is negative");
+//}
+//else
+//{
+//    Console.Write("Number is zero");
+//}
+
+
+
+//Console.Write("Enter the age:");
+//int a = int.Parse(Console.ReadLine());
+//if (a >= 0 && a <= 12)
+//{
+//    Console.Write("User is kid");
+//}
+//else if (a >= 13 && a <= 19)
+//{
+//    Console.Write("User is teenager");
+//}
+
+//else if (a >= 20)
+//{
+//    Console.Write("User is adult");
+//}
+//else
+//{
+//    Console.Write("Enter true number");
+//}
+
+
+
+//Console.Write("Enter first number:");
+//    int a = int.Parse(Console.ReadLine());
+//Console.Write("Enter second number:");
+//int b=int.Parse(Console.ReadLine());
+//if(a > b)
+//{
+//    Console.Write("First number is bigger than second number");
+//}
+//else if (a < b)
+//{
+//    Console.Write("Second number is bigger than first number");
+//}
+//else
+//{
+//    Console.Write("They are equal");
+
+
+
+//Console.Write("Enter number:");
+//int a=int.Parse(Console.ReadLine());
+//if (a == 1)
+//{
+//    Console.Write("Bazar ertesi");
+//}
+//else if (a == 2)
+//{
+//    Console.Write("Cersenbe axsami");
+//}
+//else if (a == 3)
+//{
+//    Console.Write("Cersenbe");
+//}
+//else if (a == 4)
+//{
+//    Console.Write("Cume axsami");
+//}
+//else if (a == 5)
+//{
+//    Console.Write("Cume");
+//}
+//else if (a == 6)
+//{
+//    Console.Write("Senbe");
+//}
+//else if (a == 7)
+//{
+//    Console.Write("Bazar");
+//}
+//else
+//{
+//    Console.Write("Bele gun yoxdur");
+//}
+
+
+
+//Console.Write("Enter the year:");
+//int a=int.Parse(Console.ReadLine());
+//if (a % 4 == 0 && a % 100 != 0)
+//{
+//    Console.Write("366 gun var" );
+
+//}
+//else
+//{
+//    Console.Write("365 gun var");
+//}
+
+
+//Console.Write("Enter the password:");
+//int a=int.Parse(Console.ReadLine());
+//if (a == 1881)
+//{
+//    Console.Write("Password is true");
+//}
+//else
+//{
+//    Console.Write("Password is not true");
+//}
+
+
+//Console.Write("Enter the first side:");
+//int a=int.Parse(Console.ReadLine());
+//Console.Write("Enter the second side:");
+//int b=int.Parse(Console.ReadLine());
+//Console.Write("Enter the third side:");
+// int c=int.Parse(Console.ReadLine());
+//if (a + b > c && a + c > b && b + c > a)
+//{
+//    Console.Write("This is true triangle");
+//}
+//else
+//{
+//    Console.Write("This is not true triangle");
+//}
+
+
+//Console.Write("Enter the number:");
+//int a=int.Parse(Console.ReadLine());
+//if (a > 999 && a < 10000)
+//{
+//    int r4 = a % 10;
+//    a = a / 10;
+//    int r3 = a % 10;
+//    a = a / 10;
+//    int r2 = a % 10;
+//    a = a / 10;
+//    int r1 = a % 10;
+//    a = a / 10;
+//    Console.WriteLine(r1 +r2 + r3 + r4);
+//}
+//else
+//{
+//    Console.Write("4 reqemli deyil");
+//}
